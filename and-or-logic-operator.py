53da2aca1dedd8138dba48f3490f3da05e934129
GabrielPlcs <121345542+GabrielPlcs@users.noreply.github.com>
2023-03-15 22:32:00 -0400

    Add files via upload

---
+++ and-or-logic-operator.py
@@ -0,0 +1,5 @@
+edad = int(input("Introduce tu edad: "))
+if (edad >= 20 and edad < 30) or (edad >= 30 and edad <40):
+	print("Dentro de rango (20s) o (30s)")
+else:
+	print("No está dentro de los 20s ni 30s")
